38f741b9083eaecaf13e7c6752a4ec84afebed78
MinotaroK <kyutyo.cow@gmail.com>
2015-08-07 16:49:23 +0900

    修正。

--- 04.py
+++ 04.py
@@ -2,6 +2,19 @@ import re
 
 text = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 text_list = re.split(", | ", text)
+#text = text.replace(".", "")
+#text_list = text.split(" ")
+text_index = {}
 
-for index,value in enumerate(text_list):
-	print index+1,value[0:1]
+for i, text_list in enumerate(text_list):
+	n = i + 1
+	if n in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
+		text_index[text_list[:1]] = n
+	else:
+		text_index[text_list[:2]] = n
+
+
+print(text_index)
+
+#for index,value in enumerate(text_list):
+#	print index+1,value[0:1]
