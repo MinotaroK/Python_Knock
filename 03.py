11198b23c456b472466b8a72af01bdeea49c3713
MinotaroK <kyutyo.cow@gmail.com>
2015-08-10 06:32:51 +0900

    ピリオド問題

--- 03.py
+++ 03.py
@@ -1,7 +1,10 @@
 import re
 
 text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+text_list = text.replace(".", "")
 text_list = re.split(", | ", text)
+#text = text.replace(".", "")
+#text = text.split(", | ")
 
 for index,value in enumerate(text_list):
 	print len(value)
