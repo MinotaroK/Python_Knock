00c924e3b2ee84b54e936587af5dc79b01a10733
MinotaroK <kyutyo.cow@gmail.com>
2015-08-10 14:52:31 +0900

    相変わらず動かない

--- 09.py
+++ 09.py
@@ -5,9 +5,10 @@ def randomly(text):
 	text_list = re.split("", text)
 	changed = ""
 	for i in text_list:
-		if len(text_list) >= 5
-			text_list = random.shuffle(text_list)
-		changed += text_list
+		n = n + 1
+		if len(text_list[n]) >= 5
+			text_list[n] = random.shuffle(text_list[n])
+		changed += text_list[n]
 	return changed
 
 text = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
