52b6e04765fd46e5799cd63a9efac4f4b3316cea
MinotaroK <kyutyo.cow@gmail.com>
2015-08-10 14:36:42 +0900

    動かん。たぶん書き方違う

---
+++ 09.py
@@ -0,0 +1,13 @@
+import random
+
+def randomly(text):
+	changed = ""
+	for char in word:
+		if len(str(char)) >= 5
+			char = random.shuffle(char)
+		changed += char
+	return changed
+
+text = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+
+print randomly(text)
