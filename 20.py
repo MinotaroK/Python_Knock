73ecc4461659813d0ca3fff01bd31b30b85732b3
MinotaroK <kyutyo.cow@gmail.com>
2015-08-17 20:06:13 +0900

    なんとか

---
+++ 20.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+
+import gzip
+import json
+
+f = gzip.open('./jawiki-country.json.gz', 'rb')
+file_gz = f.read()
+f.close()
+
+article = {}
+
+for line in file_gz.splitlines():
+    at_line = json.loads(line)
+    article[at_line['title']] = at_line
+
+england = article[u'イギリス']['text']
+
+print england
